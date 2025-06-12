d3ad9e61f687fafbb5a196102474b6ffd17f3a81
JPisOP@007 <balapradeep31@gmail.com>
2025-06-12 11:07:02 +0530

    env

--- online_judge/settings.py
+++ online_judge/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 # === MIDDLEWARE ===
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,7 +112,8 @@ USE_TZ = True
 # === STATIC & MEDIA ===
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
