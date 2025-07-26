2e58abd18db8c797bd9c1df39feafe79057f220e
JPisOP@007 <balapradeep31@gmail.com>
2025-07-26 12:17:54 +0530

    profile

--- core/models.py
+++ core/models.py
@@ -12,7 +12,7 @@ class UserProfile(models.Model):
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
-    photo = models.ImageField(upload_to='media/profile_photos/', null=True, blank=True)
+    photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
 
     def __str__(self):
         return f"{self.user.username} ({self.role})"
